eea95ae29da72ce3471f5dffadd285581dd47639
Gary J <github@accu.ca>
2025-02-12 23:19:54 -0800

    Update seeds

--- src/database/seeds.sql
+++ src/database/seeds.sql
@@ -1,9 +1,6 @@
 
 INSERT INTO urls (url_id, long_url, usage_count)
 VALUES
-  ('abc443', 'https://www.example.com', 5),
-  ('xyz876', 'https://www.google.com', 15),
-  ('abc663', 'https://www.example.org', 5),
-  ('abc323', 'https://www.example.net', 5),
-  ('xyz888', 'https://www.amazon.com', 0)
+  ('abc443', 'https://www.example.com', 0),
+  ('xyz876', 'https://www.google.com', 0)
   ;
