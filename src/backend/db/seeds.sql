f00c5005a4ea7966ccf39e467e26753701e60b9c
Gary J <github@accu.ca>
2025-02-12 22:19:30 -0800

    Remove duplicate seed files

--- src/backend/db/seeds.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO urls (url_id, long_url, usage_count)
-VALUES
-  ('abc123', 'https://www.example.com', 5),
-  ('xyz876', 'https://www.google.com', 15),
-  ('xyz876', 'https://www.amazon.com', 0),
-  ;
